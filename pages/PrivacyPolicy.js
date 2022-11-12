e030c1a6b4e35100a511ea2d6cf3b528bb786f09
unknown <mh.hussam.kheami@gmail.com>
2022-11-12 09:03:22 -0800

    implement privacy

---
+++ pages/PrivacyPolicy.js
@@ -0,0 +1,130 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+import styles from '../sass/FooterPages.module.scss'
+import Footer from '../components/Footer/Footer'
+
+const PrivacyPolicy = () => {
+    return (
+        <div className={styles.container}>
+            <Navbar />
+            <h1 className={styles.title}>Privacy Policy</h1>
+            <div className={styles.body}>
+                <p> Time2Travel., Part of Innovation Factory LLC (“we” or “us”) values you as our customer and recognizes that privacy is important to you.
+                    This Privacy Statement explains how we collect, use, and disclose data when you use our platform and associated services,
+                    your rights in determining what we do with the information that we collect or hold about you and tells you how to contact us.
+                </p>
+                <h2>Privacy Statement Summary</h2>
+                <p>This is a summary of our Privacy Statement. To review our Privacy Statement in full, please click here, or scroll down.</p>
+
+                <h3>What does this Privacy Statement cover?</h3>
+                <p>This Privacy Statement is designed to describe:</p>
+                <ul>
+                    <li>How and what type of personal information we collect and use</li>
+                    <li>When and with whom we share your personal information</li>
+                    <li>What choices you can make about how we collect, use, and share your personal information</li>
+                    <li>How you can access and update your information.</li>
+                </ul>
+
+                <h3>What personal information do we collect and use, and how do we collect it?</h3>
+                <p>We collect personal information when:</p>
+                <ul>
+                    <li>You give us the information</li>
+                    <li>We collect it automatically</li>
+                    <li>We receive it from others</li>
+                </ul>
+
+
+                <p>When you create an account on one of our sites, sign up to receive offers or information,
+                    or make a booking using our platform, you give us your personal information. We also collect such
+                    information through automated technology such as cookies placed on your browser, with your consent where applicable, when you visit our sites or download and use our Apps. We also receive information from affiliated companies within Innovation Factory, as well as business partners and other third parties, which help us improve our platform and associated
+                    tools and services, update and maintain accurate records, potentially detect and
+                    investigate fraud, and more effectively market our services</p>
+
+                <h3>How is your personal information shared?</h3>
+                <p>Your personal information may be shared to help you book your travel and/or vacation,
+                    assist with your travel and/or vacation stay, communicate with you
+                    (including when we send information on products and services or enable you to communicate with travel providers and/or property owners)
+                    , and comply with the law. The full Privacy Statement details how personal information is shared below.
+                </p>
+
+
+                <h3>What are your rights and choices?</h3>
+                <p>You can exercise your data protection rights in various ways. For example,
+                    you can opt out of marketing by clicking the “unsubscribe” link in the emails,
+                    in your account as applicable, or contacting our customer service.
+                    Our Privacy Statement has more information about the options and data protection rights and choices available to you.</p>
+
+
+                <h3>How to contact us</h3>
+                <p>More information about our privacy practices is in our full Privacy Statement.
+                    You can also contact us as described below in the “Contact Us” section to ask questions about how we handle your personal
+                    information or make requests about your personal information.</p>
+
+
+                <h2>Categories of Personal Information We Collect</h2>
+                <p>When you use our platform, Apps, or associated tools or services, we may collect the following kinds of personal information from you as needed:</p>
+                <ul>
+                    <li>Name, username, email address, telephone number, and home, business, and billing addressees (including street and postal code)</li>
+                    <li>Government issued Identification required for booking or identity verification, such as passport, driver’s license, government redress numbers, and country of residence (for travel insurance purposes), and for vacation property owners, tax identification number,
+                    </li>
+                    <li>Payment information such as payment card number, expiration date, billing address, and financial account number
+                    </li>
+                    <li>Travel-related preferences and requests such as favorite destination and accommodation types, and special dietary and accessibility needs, as available
+                    </li>
+                    <li>Loyalty program and membership information
+                    </li>
+                    <li>Birth date and gender
+                    </li>
+                    <li>Geolocation
+                    </li>
+                    <li>Images (including facial photographs), videos, and other recordings</li>
+                    <li>Social media account ID and other publicly available information
+                    </li>
+                    <li>Communications with us (such as recordings of calls with customer service representatives for quality assurance and training purposes)</li>
+                    <li>Searches you conduct, transactions, and other interactions with you on our online services and Apps
+                    </li>
+                    <li>Other communications that occur through the platform among partners and travelers,
+                        and in-group chat and traveler-collaboration tools
+                    </li>
+                    <li>The searches and transactions conducted through the platform
+                    </li>
+                    <li>Data you give us about other people, such as your travel companions or others for whom you are making a booking</li>
+                    <li>Information we receive about you from other Innovation Factory companies and third parties such as our business and affiliate partners and authorized service providers which may include updated contact information, demographic information, interests, and purchase history, which we may add to your account or profile and use for market research and analysis
+                        When you install any of our apps or use our platform, we automatically collect the following types of information from your device:
+                    </li>
+                    <ul>
+                        <li>IP address</li>
+                        <li>Device type</li>
+                        <li>Unique device identification numbers</li>
+                        <li>Internet browser-type (such as Firefox, Safari, Chrome, and Internet Explorer)
+                        </li>
+                        <li>Internet Service Provider
+                        </li>
+                        <li>Operating System
+                        </li>
+                        <li>Mobile carrier</li>
+                        <li>How your device has interacted with our online services, including the pages accessed, links clicked, trips viewed, and features used, along with associated dates and times
+                        </li>
+                        <li>
+                            Details of any referring website or exit pages, as well as general geographic location (such as at the country or city-level)
+                            Our Cookie Statement section, included in this Privacy Statement, further explains how we use cookies and similar tracking technology.
+
+                        </li>
+                 
+
+
+                    </ul>
+
+                </ul>
+
+
+
+
+
+            </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default PrivacyPolicy
