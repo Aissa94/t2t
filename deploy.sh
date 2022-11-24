313aa7acfef1167d3c1b247c4acdb4cd6e39f27b
Aissa94 <aissabelkaid94@gmail.com>
2022-11-24 05:17:12 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-echo "hello world"
+IMAGE_NAME="aissa94/nextjs-docker"
+IMAGE_TAG=$(git rev-parse --short HEAD) # first 7 characters of the current commit hash
+
+echo "Building Docker image ${IMAGE_NAME}:${IMAGE_TAG}, and tagging as latest"
+docker build -t "${IMAGE_NAME}:${IMAGE_TAG}" .
+docker tag "${IMAGE_NAME}:${IMAGE_TAG}" "${IMAGE_NAME}:latest"
+
+echo "Authenticating and pushing image to Docker Hub"
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+docker push "${IMAGE_NAME}:${IMAGE_TAG}"
+docker push "${IMAGE_NAME}:latest"
